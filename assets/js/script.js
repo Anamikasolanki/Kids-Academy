e8c6837ffa3d0efe36301193a738a62133b603e3
Anamikasolanki <anamikasolnki404@gmail.com>
2024-09-10 17:22:27 +0530

    projects completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,106 @@
 var countDownDate = new Date("Oct 8, 2024 00:00:00").getTime();
-var x = setInterval(function(){
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 
-    if(distance<0){
-        clearInterval(x);
-        document.getElementById("days").innerHTML = "00";
-        document.getElementById("hours").innerHTML = "00";
-        document.getElementById("minutes").innerHTML = "00";
-        document.getElementById("seconds").innerHTML = "00";    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
+
+//  Script.js
+const rangevalue = document.querySelector(".slider-container .price-slider");
+const rangeInputvalue = document.querySelectorAll(".range-input input");
+
+// Set the price gap
+let priceGap = 500;
+
+// Adding event listners to price input elements
+const priceInputvalue = document.querySelectorAll(".price-input input");
+for (let i = 0; i < priceInputvalue.length; i++) {
+  priceInputvalue[i].addEventListener("input", (e) => {
+    // Parse min and max values of the range input
+    let minp = parseInt(priceInputvalue[0].value);
+    let maxp = parseInt(priceInputvalue[1].value);
+    let diff = maxp - minp;
+
+    if (minp < 0) {
+      alert("minimum price cannot be less than 0");
+      priceInputvalue[0].value = 0;
+      minp = 0;
     }
 
-},1000);
+    // Validate the input values
+    if (maxp > 10000) {
+      alert("maximum price cannot be greater than 10000");
+      priceInputvalue[1].value = 10000;
+      maxp = 10000;
+    }
+
+    if (minp > maxp - priceGap) {
+      priceInputvalue[0].value = maxp - priceGap;
+      minp = maxp - priceGap;
+
+      if (minp < 0) {
+        priceInputvalue[0].value = 0;
+        minp = 0;
+      }
+    }
+
+    // Check if the price gap is met
+    // and max price is within the range
+    if (diff >= priceGap && maxp <= rangeInputvalue[1].max) {
+      if (e.target.className === "min-input") {
+        rangeInputvalue[0].value = minp;
+        let value1 = rangeInputvalue[0].max;
+        rangevalue.style.left = `${(minp / value1) * 100}%`;
+      } else {
+        rangeInputvalue[1].value = maxp;
+        let value2 = rangeInputvalue[1].max;
+        rangevalue.style.right = `${100 - (maxp / value2) * 100}%`;
+      }
+    }
+  });
+
+  // Add event listeners to range input elements
+  for (let i = 0; i < rangeInputvalue.length; i++) {
+    rangeInputvalue[i].addEventListener("input", (e) => {
+      let minVal = parseInt(rangeInputvalue[0].value);
+      let maxVal = parseInt(rangeInputvalue[1].value);
+
+      let diff = maxVal - minVal;
+
+      // Check if the price gap is exceeded
+      if (diff < priceGap) {
+        // Check if the input is the min range input
+        if (e.target.className === "min-range") {
+          rangeInputvalue[0].value = maxVal - priceGap;
+        } else {
+          rangeInputvalue[1].value = minVal + priceGap;
+        }
+      } else {
+        // Update price inputs and range progress
+        priceInputvalue[0].value = minVal;
+        priceInputvalue[1].value = maxVal;
+        rangevalue.style.left = `${(minVal / rangeInputvalue[0].max) * 100}%`;
+        rangevalue.style.right = `${
+          100 - (maxVal / rangeInputvalue[1].max) * 100
+        }%`;
+      }
+    });
+  }
+}
